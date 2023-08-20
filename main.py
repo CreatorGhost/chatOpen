dec039617941d70fbdb84ccb1be409a4798d7bba
Aditya Pratap Singh <cybercreed010@gmail.com>
2023-08-20 14:41:59 +0530

    Added support to make it a news anchor

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import os
 import openai
 from dotenv import load_dotenv
-
+from pprint import pprint as print
 load_dotenv()
 
 openai.api_key = os.environ['OPENAI_API_KEY']
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {
-      "role": "user",
-      "content": ""
-    }
-  ],
-  temperature=1,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-print(response)
+
+
+def generate_artile(tile):
+    response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=[
+                    {
+                    "role": "system",
+                    "content": "You are very knowlegeble ai that helps people on witing new artile and you artile follows all the good practices"
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Write an artile about {tile}"
+                    }
+                ]
+                )
+    return response["choices"][0]["message"]["content"]
+
+
+
+title = "2008 Market Crash Report"
+
+res = generate_artile(title)
+
+print(res)
